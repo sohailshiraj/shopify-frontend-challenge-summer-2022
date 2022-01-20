63db7816b510ef2486c793c140c0148dfd383204
Sohail Shiraj <sohail.shiraj@hotmail.com>
2022-01-19 20:37:22 -0500

    Added search date filter, added some changes for style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,11 @@ import { MatButtonModule} from '@angular/material/button';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +31,11 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatProgressSpinnerModule,
     MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     NgbModule
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { Photo } from './model';
 import { HomeService } from './service/home.service';
 
+import * as moment from 'moment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,21 +11,44 @@ import { HomeService } from './service/home.service';
 })
 export class HomeComponent implements OnInit {
 
+  @Output() dateChange: EventEmitter<MatDatepickerInputEvent<any>> = new EventEmitter();
+
   public roverPhotos: Photo[] = [];
+  public backupPhotos: Photo[] = [];
   public isDataLoading: boolean = false;
+  public selectedDate: any;
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
-    this.isDataLoading = true;
-    //this.getRoversImagesBySol(1000)
+    this.isDataPresentLocally();
   }
 
-  public getRoversImagesBySol(sol: number): void {
+  private isDataPresentLocally(){
+    if(localStorage.getItem("photoData") != null){
+      const photos = localStorage.getItem("photoData");
+      this.roverPhotos = photos !== null ? JSON.parse(photos): [];
+      this.backupPhotos = photos !== null ? JSON.parse(photos): [];
+    }else {
+      this.getRoversImagesBySol(1000);
+    }
+  }
+
+  private saveDataLocally(){
+    localStorage.setItem("photoData", JSON.stringify(this.roverPhotos))
+  }
+
+  private deleteDataLocally(){
+    localStorage.removeItem("photoData");
+  }
+
+  private getRoversImagesBySol(sol: number): void {
     this.isDataLoading = true;
     this.homeService.getMarsRoverPhotosBySol(sol).subscribe(
       (res) => {
         console.log(res);
         this.roverPhotos = res['photos'];
+        this.backupPhotos = res['photos'];
+        this.saveDataLocally();
         this.isDataLoading = false;
       },
       (err) => {
@@ -34,10 +59,33 @@ export class HomeComponent implements OnInit {
 
   public likePost(index: number){
     this.roverPhotos[index].liked = true;
+    this.saveDataLocally();
   }
 
   public unlikePost(index: number){
     this.roverPhotos[index].liked = false;
+    this.saveDataLocally();
+  }
+
+  public refreshData() {
+    this.deleteDataLocally();
+    this.isDataPresentLocally();
+    this.selectedDate = null;
+  }
+
+  public onChangingDate(e: any){
+    console.log(e.value);
+    this.isDataLoading = true;
+    this.roverPhotos = this.backupPhotos;
+    console.log(this.roverPhotos.length);
+    this.roverPhotos = this.roverPhotos.filter((item) => {
+      var earth_date = moment(item.earth_date, 'YYYY-MM-DD');
+      var selectedDate = moment(e.value);
+      return earth_date >= selectedDate;
+    });
+    console.log(this.roverPhotos.length);
+
+    this.isDataLoading = false;
   }
 
 }
