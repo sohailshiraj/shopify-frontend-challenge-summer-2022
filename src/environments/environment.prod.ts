3cd12afdb36ad41156e6e03fdbdd81e936358387
Sohail Shiraj <sohail.shiraj@hotmail.com>
2022-01-19 21:27:48 -0500

    added api key in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serviceURL: "https://api.nasa.gov/mars-photos/api/v1/"
+  serviceURL: "https://api.nasa.gov/mars-photos/api/v1/",
+  apiKey: "DEMO_KEY"
 };
